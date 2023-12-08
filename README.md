e239d7cf71c8757c1bc57f80a6d10eec309f9003
ddewaele <ddewaele@gmail.com>
2023-12-08 10:54:26 +0100

    intial ocmmit

--- README.md
+++ README.md
@@ -1,6 +1,47 @@
-# Welcome to your CDK TypeScript project
+# cdk-simple ec2
 
-This is a blank project for CDK development with TypeScript.
+This repository contains a Cloud Development Kit (CDK) stack that automates the deployment of a simple Amazon EC2 instance with a configured security group for SSH access. This CDK stack is ideal for users who want to quickly set up a basic EC2 instance in AWS with the necessary configurations for web serving and SSH access.
+
+## Features
+
+- Default VPC Selection: Automatically selects the default VPC for deployment.
+- Security Group Configuration: Sets up a security group to allow SSH (TCP port 22) and HTTP (TCP port 80) inbound traffic.
+- Role and Policy Assignment: Creates an EC2 role with AmazonS3ReadOnlyAccess managed policy.
+- User Data Script: Includes a user data script to install necessary packages and set up a basic HTTP server.
+- EC2 Instance Creation: Deploys a t2.micro EC2 instance with the latest Amazon Linux AMI and attaches the created role and security group.
+
+## Prerequisites
+
+- AWS account with appropriate permissions to create the described resources.
+- AWS CDK installed and configured on your machine.
+- Basic understanding of TypeScript and AWS CDK.
+
+## Installation & Deployment
+
+- Clone the repository to your local machine.
+- Navigate to the repository directory.
+- Run npm install to install necessary dependencies.
+- Deploy the stack to your AWS account using cdk deploy.
+
+
+## Usage
+
+After deployment, the EC2 instance will be accessible via SSH. The HTTP server running on the instance will display a simple "Hello World" message.
+
+## Customization
+
+You can modify the stack to suit your specific requirements. For instance, changing the instance type, machine image, or adjusting the user data script.
+
+## Security
+
+Ensure that your AWS credentials and key pairs are managed securely. Review and modify the security group rules according to your security policies.
+
+## Support
+
+For issues or enhancements, please open an issue in the repository.
+
+Note: This README assumes that users have basic knowledge of AWS services and CDK. It is important to keep security best practices in mind when using this stack in a production environment.
+
 
 The `cdk.json` file tells the CDK Toolkit how to execute your app.
 
--- lib/cdk-simple-ec2-stack.ts
+++ lib/cdk-simple-ec2-stack.ts
@@ -1,16 +1,67 @@
 import * as cdk from 'aws-cdk-lib';
+import { Instance, InstanceType, MachineImage, Peer, Port, SecurityGroup, UserData, Vpc } from 'aws-cdk-lib/aws-ec2';
+import { ManagedPolicy, Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkSimpleEc2Stack extends cdk.Stack {
+
+  instance: Instance;
+  sshSecurityGroup: SecurityGroup;
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkSimpleEc2Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const vpc = Vpc.fromLookup(this, "VPC", {
+      isDefault: true,
+    });
+
+    // Create a security group for SSH
+    this.sshSecurityGroup = new SecurityGroup(this, 'SSHSecurityGroup', {
+      vpc: vpc,
+      description: 'Security Group for SSH',
+      allowAllOutbound: true,
+    });
+
+    // Allow SSH inbound traffic on TCP port 22
+    this.sshSecurityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(22));
+    this.sshSecurityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(80));
+
+
+    // Create reference to desired policy, in this case I want to set up a host that I can connect to through AWS Session Manager
+    var instanceProfile = ManagedPolicy.fromAwsManagedPolicyName("AmazonS3ReadOnlyAccess");
+
+    // Create the role resource
+    const role = new Role(this, "bastion-role", {
+      assumedBy: new ServicePrincipal("ec2.amazonaws.com"),
+    });
+
+    // Add the policy to the role
+    role.addManagedPolicy(instanceProfile);
+
+    const userData = UserData.forLinux();
+
+      userData.addCommands(
+      'yum update -y',
+      'yum update -y',
+      'dnf update -y',
+      'dnf install java-21-amazon-corretto -y',
+      'yum install -y httpd',
+      'systemctl start httpd',
+      'systemctl enable httpd',
+      'echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html',
+    );
+
+    // Create the EC2 instance
+    this.instance = new Instance(this, 'Instance', {
+      vpc: vpc,
+      instanceType: new InstanceType('t2.micro'),
+      machineImage: MachineImage.latestAmazonLinux2023(),
+      userData: userData,
+      securityGroup: this.sshSecurityGroup,
+      role:role,
+      keyName: 'ec2-test-keypair'
+    })
   }
+
 }
