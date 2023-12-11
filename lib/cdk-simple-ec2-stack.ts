ae2df09b18044702693f4e1c3861ddcc9ac9d9de
ddewaele <ddewaele@gmail.com>
2023-12-11 13:05:56 +0100

    Added region in output

--- lib/cdk-simple-ec2-stack.ts
+++ lib/cdk-simple-ec2-stack.ts
@@ -49,7 +49,9 @@ export class CdkSimpleEc2Stack extends cdk.Stack {
       'yum install -y httpd',
       'systemctl start httpd',
       'systemctl enable httpd',
-      'echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html',
+      'TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`',
+      'REGION=`curl -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/meta-data/placement/region`',
+      'echo "<h1>Hello World from $(hostname -f) in ${REGION} </h1>" > /var/www/html/index.html',
     );
 
     // Create the EC2 instance
